437492a5f479de5011a95f68208e8681c901c02d
Kees Goedegebuure <keesgoed@hotmail.com>
2016-04-15 13:37:52 +0200

    Add 'project aanmaken' (not complete)

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -10,9 +10,13 @@ public function view()
     {
 		$this->loginstate->logged_out();
     }
-public function projectpagina($page = 'projectpagina'){
+public function projectpagina(){
     $this->load->view('templates/header');
     $this->load->view('pages/projectpagina');
     }
+public function project_aanmaken(){
+    $this->load->view('templates/header');
+    $this->load->view('pages/projectaanmaken');
 
+    }
 }
