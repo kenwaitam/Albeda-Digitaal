3bfd3ee2d5d077a39a6b64c78131a2f12a7928fa
Jurgen van der Heiden <Jurgen van der Heiden>
2016-04-15 09:25:49 +0200

    Lightbox jonge!

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -15,6 +15,9 @@
     <!-- Bootstrap -->
     <?php echo link_tag('css/bootstrap.min.css'); ?>
 
+     <!-- Voor image -->
+    <?php echo link_tag('css/lightbox.css'); ?>
+
     <!-- Custom CSS -->
     <?php echo link_tag('css/custom.css'); ?>
 
@@ -55,6 +58,59 @@
             ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
             var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
         })();
+    </script>
+    <!-- Lightbox  -->
+    <script type="text/javascript" src="<?php echo base_url();?>js/lightbox.js" ></script>
+    <script type="text/javascript">
+        /* When the user clicks on the button,
+         toggle between hiding and showing the dropdown content */
+        function drop_menu() {
+            document.getElementById("dropdown").classList.toggle("show");
+        }
+
+        // Close the dropdown menu if the user clicks outside of it
+        window.onclick = function(event) {
+            if (!event.target.matches('.dropbtn')) {
+
+                var dropdowns = document.getElementsByClassName("dropdown-content");
+                var i;
+                for (i = 0; i < dropdowns.length; i++) {
+                    var openDropdown = dropdowns[i];
+                    if (openDropdown.classList.contains('show')) {
+                        openDropdown.classList.remove('show');
+                    }
+                }
+            }
+        }
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-7243260-2']);
+        _gaq.push(['_trackPageview']);
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
+
+    <!-- Voor lightbox -->
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('.fancybox').fancybox();
+            $("#fancybox-manual-a").click(function() {
+                $.fancybox.open('1_b.jpg');
+            });
+        });
+        // Change title type, overlay closing speed
+            $(".fancybox-effects-a").fancybox({
+                helpers: {
+                    title : {
+                        type : 'outside'
+                    },
+                    overlay : {
+                        speedOut : 0
+                    }
+                }
+            });
     </script>
 </head>
 
