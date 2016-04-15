19d5a430d469618c74618ef4b8092c072f413c07
Kees Goedegebuure <keesgoed@hotmail.com>
2016-04-15 09:46:17 +0200

    Add projectpagina

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -6,27 +6,13 @@ class Pages extends CI_Controller{
 		$this->load->library('loginstate');
 	}
 
-public function view($page = 'home')
+public function view()
     {
-		$this->loginstate->logged_out($page = 'home');
-        /*if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
-        {
-            // Whoops, we don't have a page for that!
-            show_404();
-        }
-
-        $this->load->helper('html');
-
-        $data['title'] = ucfirst($page); // Capitalize the first letter
-
-        $this->load->view('templates/header', $data);
-        $this->load->view('pages/'.$page, $data);
-        $this->load->view('pages/deelnemers', $data);
-        $this->load->view('pages/fotos', $data);
-        $this->load->view('pages/videos', $data);
-        $this->load->view('pages/documenten', $data);
-        $this->load->view('pages/contact', $data);
-        $this->load->view('templates/footer', $data);
-        $this->load->view('templates/cirkelmenu', $data);*/
+		$this->loginstate->logged_out();
     }
+public function projectpagina($page = 'projectpagina'){
+    $this->load->view('templates/header');
+    $this->load->view('pages/projectpagina');
+    }
+
 }
---
+++ application/views/pages/projectpagina.php
@@ -0,0 +1,33 @@
+
+<div class="container">
+    <h3>Projectpagina</h3>
+    <div class="row">
+        <div class="col-lg-3 project1">
+            <h4>Te laat systeem</h4>
+            <img src="<?php echo base_url("img/nfcimg.jpg")?>" height="200" width="230">
+            <p></p>
+            <p>Er moet een systeem komen waarmee te laat briefjes sneller en makkelijker worden gemaakt.</p>
+        </div>
+        <div class="col-lg-1"></div>
+        <div class="col-lg-3 project2">
+            <h4>Sample Project</h4>
+            <img src="<?php echo base_url("img/placeholder_project.png")?>" height="200" width="230">
+            <p></p>
+            <p>Sample project to fill up the page</p>
+        </div>
+        <div class="col-lg-1"></div>
+        <div class="col-lg-3 project3">
+            <h4>Sample Project</h4>
+            <img src="<?php echo base_url("img/placeholder_project.png")?>" height="200" width="230">
+            <p></p>
+            <p>Sample project to fill up the page</p>
+        </div>
+    </div>
+</div>
+<script>
+    $(document).ready(function(){
+       $('.project1').click(function(){
+           $(location).attr('href', '../')
+       });
+    });
+</script>
