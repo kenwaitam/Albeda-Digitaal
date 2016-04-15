b997a2070d54ecfb42a1c3927ddd79d539567b37
Jurgen van der Heiden <Jurgen van der Heiden>
2016-04-15 12:16:41 +0200

    no message

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -1,13 +1,42 @@
-<h1>Albeda Digitaal Login</h1>
-
-<?php echo form_open('login/index'); ?>
-
-<?php echo validation_errors(); ?>
-
-<p>Username</p>
-<?php echo form_input('username'); ?>
-
-<p>Password</p>
-<?php echo form_password('password'); ?> <br />
-
-<?php echo form_submit('submit', 'Sign in', 'input style="margin-top: 15px;"'); ?> <br />
+<div class="container-fluid main-container-login">
+	<div class="container container-login">
+	  <div class="row">
+		    <div class="col-sm-12">
+				<h1>Albeda Digitaal Login</h1>
+			</div>
+		</div>
+		<?php echo form_open('login/index', 'class="form-inline"'); ?>
+		<div class="row">
+		    <div class="col-sm-12">
+				<?php echo validation_errors(); ?>
+			</div>
+		</div>
+		<div class="row">
+		    <div class="col-sm-12">
+				<div class="col-sm-12">
+					<label class="control-label">Gebruikersnaam</label>
+				</div>
+				<div class="col-sm-12">
+				<?php echo form_input('username','','class="form-control input-lg"'); ?>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+		    <div class="col-sm-12">
+				<div class="col-sm-12">
+					<label class="control-label">Wachtwoord</label>
+				</div>
+				<div class="col-sm-12">
+				<?php echo form_password('password','','class="form-control input-lg"'); ?>
+				</div>
+			</div>
+		</div>	
+		<div class="row">
+		    <div class="col-sm-12">
+				<div class="col-sm-12">
+				<?php echo form_submit('submit ', 'Sign in', 'class="btn btn-lg btn-login" style="margin-top: 15px;"'); ?>
+				</div>
+			</div>
+		</div>	
+	</div>
+</div>
--- application/views/login/loginfail.php
+++ application/views/login/loginfail.php
@@ -1,13 +1,14 @@
 <h1><?php echo $failtitle; ?></h1>
+<div class="container">
+	<?php echo form_open('login/index'); ?>
 
-<?php echo form_open('login/index'); ?>
+	<?php echo validation_errors(); ?>
 
-<?php echo validation_errors(); ?>
+	<p>Username</p>
+	<?php echo form_input('username'); ?>
 
-<p>Username</p>
-<?php echo form_input('username'); ?>
+	<p>Password</p>
+	<?php echo form_password('password'); ?> <br />
 
-<p>Password</p>
-<?php echo form_password('password'); ?> <br />
-
-<?php echo form_submit('submit', 'Sign in', 'input style="margin-top: 15px;"'); ?> <br />
+	<?php echo form_submit('submit', 'Sign in', 'input style="margin-top: 15px;"'); ?> <br />
+</div>
