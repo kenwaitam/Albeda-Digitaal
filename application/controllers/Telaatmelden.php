2ce9754369d270626748c8052c963ba01bf5051b
Jurgen van der Heiden <Jurgen van der Heiden>
2016-04-22 13:07:02 +0200

    Te laat formulier update 0.1(nog niet werkend)

--- application/controllers/Telaatmelden.php
+++ application/controllers/Telaatmelden.php
@@ -16,27 +16,33 @@ class Telaatmelden extends CI_Controller {
         $this->load->library('loginstate');
         $this->load->helper('form');
         $this->load->library('form_validation');
-
+            if(isset($_POST['submit']))
+                $this->setPostValues();
+                $this->makeLateReport();
     }
-
+    private function makeLateReport(){
+         // haal de waardes op
+        $this->form_validation->set_rules('firstname', 'Firstname', 'required');
+        $this->form_validation->set_rules('insertion', 'Insertion', '');
+        $this->form_validation->set_rules('lastname', 'Lastname', 'required');
+        $this->form_validation->set_rules('studentnumber', 'studentnumber', 'required');
+        $this->form_validation->set_rules('mentor', 'Mentor', 'required');
+        $this->form_validation->set_rules('reason', 'Reason', 'required');
+             if($this->form_validation->run())
+                echo "t";
+             else
+                echo "b";
+    }
+    private function setPostValues(){
+            $this->firstname = $this->input->post('firstname');
+            $this->insertion = $this->input->post('insertion');
+            $this->lastname = $this->input->post('lastname');
+            $this->sudentnumber = $this->input->post('studentnumber');
+            $this->mentor = $this->input->post('mentor');
+            $this->reason = $this->input->post('reason');
+    }
     public function index(){
         $this->load->view('templates/header');
         $this->load->view('project_telaat/formulier_telaat');
-
-        // haal de waardes op
-        $this->form_validation->set_rules('firstname', 'Username', 'required');
-        $this->form_validation->set_rules('insertion', 'Username', '');
-        $this->form_validation->set_rules('lastname', 'Username', 'required');
-        $this->form_validation->set_rules('sudentnumber', 'Username', 'required');
-        $this->form_validation->set_rules('mentor', 'Username', 'required');
-        $this->form_validation->set_rules('reason', 'Username', 'required');
-
-        // kijk of submit bestaat
-        if(isset($_POST["submit"])) {
-            echo "b";
-            if($this->form_validation->run()) {
-                echo "t";
-            }
-        }
     }
 }
