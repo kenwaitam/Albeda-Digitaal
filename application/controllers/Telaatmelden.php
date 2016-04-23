5c1776d7a35b0a42fcfe309a59228c4d9058cb22
Jurgen van der Heiden <Jurgen van der Heiden>
2016-04-23 14:42:42 +0200

    Basis te laat meld formulier en printen "werkt"

--- application/controllers/Telaatmelden.php
+++ application/controllers/Telaatmelden.php
@@ -6,10 +6,11 @@ class Telaatmelden extends CI_Controller {
     public $firstname;
     public $insertion;
     public $lastname;
-    public $sudentnumber;
+    public $studentnumber;
     public $mentor;
     public $reason;
     public $error_message;
+    private $print = false;
 
     public function __construct()
     {
@@ -17,9 +18,10 @@ class Telaatmelden extends CI_Controller {
         $this->load->library('loginstate');
         $this->load->helper('form');
         $this->load->library('form_validation');
-            if(isset($_POST['submit']))
+            if(isset($_POST['submit'])){
                 $this->setPostValues();
                 $this->makeLateReport();
+            }
     }
     private function makeLateReport(){
          // haal de waardes op
@@ -29,21 +31,28 @@ class Telaatmelden extends CI_Controller {
         $this->form_validation->set_rules('studentnumber', 'studentnumber', 'required');
         $this->form_validation->set_rules('mentor', 'Mentor', 'required');
         $this->form_validation->set_rules('reason', 'Reason', 'required');
-             if($this->form_validation->run())
+             if($this->form_validation->run()){
                 //volgende stap: waardes ophalen.
-             else
-                $this->error_message = "Sommige waardes konden niet worden opgehaald.";
+                 $this->print = true;
+
+             }else{
+                $this->error_message = "Sommige waardes zijn niet juist ingevuld.";
+             }
     }
     private function setPostValues(){
-            $this->firstname = $this->input->post('firstname');
-            $this->insertion = $this->input->post('insertion');
-            $this->lastname = $this->input->post('lastname');
-            $this->sudentnumber = $this->input->post('studentnumber');
-            $this->mentor = $this->input->post('mentor');
-            $this->reason = $this->input->post('reason');
+            $this->firstname = set_value('firstname');
+            $this->insertion = set_value('insertion');
+            $this->lastname = set_value('lastname');
+            $this->studentnumber = set_value('studentnumber');
+            $this->mentor = set_value('mentor');
+            $this->reason = set_value('reason');
     }
     public function index(){
         $this->load->view('templates/header');
-        $this->load->view('project_telaat/formulier_telaat');
+        if(!$this->print){
+            $this->load->view('project_telaat/formulier_telaat');
+        }else{
+            $this->load->view('project_telaat/print_formulier');
+        }
     }
 }
