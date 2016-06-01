c622a21f3cf88bf4bbeac4c50bf3ab89d5f570ab
Niels Evers <niels.evers95@gmail.com>
2016-06-01 10:31:01 +0200

    some login stuff

--- application/models/Login_Model.php
+++ application/models/Login_Model.php
@@ -8,6 +8,14 @@
 		public function login_query() {
 			$username = $this->input->post('username');
 			$password = $this->input->post('password');
+			$admin = DB::table('role_user')->where([
+				['role_id', '=', 1],
+				['user_id', '=', $user->id],
+
+				$this->db->where('name', $username);
+				$this->db->where('password', $password);
+				$this->db->join('user_role', '.id = .id');
+
 			if ($username == 'test' && $password == 'test') {
 				return 1;
 			}
@@ -18,9 +26,6 @@
 			$query = $this->db->get_where('students', array('studentnumber' => $studentcode));
 			return $query->num_rows();
 
-		}
-		public function student_data(){
-			$studentcode
 		}
 			/*$query = $this->db->get_where('login', array('loginName' => $username, 'loginPassword' => $password));
 			return $query->num_rows();*/
